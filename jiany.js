8bbcea63d45549e991709b1c5e28f0121a7a51a8
laosuyo <126240290+laosuyo@users.noreply.github.com>
2023-03-11 12:36:25 +0800

    Update jiany.js

--- jiany.js
+++ jiany.js
@@ -1,7 +1,7 @@
 /*******************************
 
 脚本名称:  简影——(解锁会员)v6.9.0
-更新时间：2023年3月7日 下午11:36
+更新时间：2023年3月7日
 温馨提示：资源来自全网搜集，如有侵权及时与我方联系：w6666999@gmail.com
 软件地址：苹果商店搜索
 更新时间：2023年3月7日
@@ -16,7 +16,7 @@ TG订阅：https://t.me/S666699999
 
 [rewrite_local]
 
-^http[s]?:\/\/api.jianmovie.com url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/jianmovie.js
+^http[s]?:\/\/api.jianmovie.com url script-response-body https://raw.githubusercontent.com/laosuyo/Quantumult-X/main/jiany.js
 
 [mitm] 
 
