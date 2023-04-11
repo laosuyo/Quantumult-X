4f9fa30b8f314c684871740bba107a1f62cc3f36
laosuyo <126240290+laosuyo@users.noreply.github.com>
2023-04-11 16:22:32 +0800

    Add files via upload

---
+++ Soul.js
@@ -0,0 +1,6 @@
+/*********************************************
+Soul解除送礼私聊限制
+[rewrite_local]
+^https:\/\/api-chat\.soulapp\.cn\/chat\/limitInfo url script-response-body https://raw.githubusercontent.com/Crazy-Z7/Scrip/main/Soul.js
+hostname = api-chat.soulapp.cn
+*********************************************/
