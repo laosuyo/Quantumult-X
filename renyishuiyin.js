3eb30127536372b55b7f7355b1e5555e853d35a6
laosuyo <126240290+laosuyo@users.noreply.github.com>
2024-09-18 13:47:18 +0800

    Add files via upload

---
+++ renyishuiyin.js
@@ -0,0 +1,23 @@
+/******************************
+
+温馨提示：资源来自全网搜集，如有侵权及时与我方联系：w6666999@gmail.com
+脚本功能：任意水印相机（永久会员）
+软件版本：1.6.1
+更新时间：2024-9-18
+微信公众号：夕阳助手科技
+更多资源：www.laosu.xyz
+TG交流群：hhttps://t.me/xyjsxb
+TG订阅：https://t.me/S666699999
+使用声明：此脚本仅供学习与交流，请勿转载与贩卖！⚠️⚠️⚠️
+
+*******************************
+[rewrite_local]
+# >任意水印相机（永久会员）
+^https?:\/\/ff.mofangzhice.com\/api\/client\/info url script-response-body https://raw.githubusercontent.com/WeiGiegie/666/main/rysyxj.js
+
+[mitm] 
+hostname = ff.mofangzhice.com
+
+*
+*
+*/
